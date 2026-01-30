d82aad4fa69bcd333a0ccbb0057388f4e19f9fc7
Joel <jdjpm21@gmail.com>
2026-01-29 22:37:41 -0500

    initial commit

---
+++ src/test/java/com/personal/loan/PersonalLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
